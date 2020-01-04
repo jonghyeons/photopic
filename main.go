78581ead20b394d685ca0999cc246510b8112caa
whdgus906 <whdgus906@gmail.com>
2020-01-04 22:22:21 +0900

    feat: Add lens report function

--- main.go
+++ main.go
@@ -3,10 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/rwcarlsen/goexif/exif"
 	"io/ioutil"
 	"os"
 	"path/filepath"
 	"strings"
+)
+
+var (
+	wideAngleLensCnt = 0
+	standardLensCnt  = 0
+	telephotoLensCnt = 0
+	errCnt           = 0
 )
 
 func main() {
@@ -20,7 +28,7 @@ func main() {
 		dir, _ = filepath.Abs(filepath.Dir(os.Args[0]))
 	}
 
-	makeDir(dir)
+	MakeDir(dir)
 	files, err := ioutil.ReadDir(dir)
 
 	if len(files) == 0 || err != nil {
@@ -38,23 +46,37 @@ func main() {
 		if fileType == "JPG" {
 			os.Rename(dir+"/"+file.Name(), dir+"/jpg/"+file.Name())
 		} else if Contains(rawType, fileType) {
+			done := SetLensAngleCnt(dir + "/" + file.Name())
+			if !done {
+				errCnt++
+			}
+
 			os.Rename(dir+"/"+file.Name(), dir+"/raw/"+file.Name())
 		}
 	}
+
+	fmt.Println("## Photopic Report")
+	fmt.Println("1. Lens Angle")
+	fmt.Println("# This data is analyzed only in the raw files. ")
+	fmt.Println("wide-angle: ", wideAngleLensCnt, "shot")
+	fmt.Println("standard: ", standardLensCnt, "shot")
+	fmt.Println("telephoto: ", telephotoLensCnt, "shot")
+	fmt.Println("analysis failure: ", errCnt)
+
 }
 
-func makeDir(path string) {
-	jpgDir, _ := exists(path + "/jpg")
+func MakeDir(path string) {
+	jpgDir, _ := Exists(path + "/jpg")
 	if !jpgDir {
 		os.MkdirAll(path+"/jpg", os.ModePerm)
 	}
-	rawDir, _ := exists(path + "/raw")
+	rawDir, _ := Exists(path + "/raw")
 	if !rawDir {
 		os.MkdirAll(path+"/raw", os.ModePerm)
 	}
 }
 
-func exists(path string) (bool, error) {
+func Exists(path string) (bool, error) {
 	_, err := os.Stat(path)
 	if err == nil {
 		return true, nil
@@ -72,4 +94,27 @@ func Contains(a []string, x string) bool {
 		}
 	}
 	return false
+}
+
+func SetLensAngleCnt(fnameWithPath string) bool {
+	f, _ := os.Open(fnameWithPath)
+
+	exif.RegisterParsers()
+	x, err := exif.Decode(f)
+	if err != nil {
+		return false
+	}
+
+	focal, _ := x.Get(exif.FocalLength)
+	numer, denom, _ := focal.Rat2(0)
+
+	if numer/denom < 35 {
+		wideAngleLensCnt++
+	} else if numer/denom >= 35 || numer/denom < 85 {
+		standardLensCnt++
+	} else {
+		telephotoLensCnt++
+	}
+
+	return true
 }

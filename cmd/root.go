107d10b4f5abe7d0304fb17df17d8e1821d75e84
jonghyeons <whdgus906@gmail.com>
2023-01-11 21:34:38 +0900

    feat: 메인 설명글에 ASCII 아트 추가

--- cmd/root.go
+++ cmd/root.go
@@ -9,7 +9,13 @@ import (
 var rootCmd = &cobra.Command{
 	Use:   "photopic",
 	Short: "Photopic is JPG, RAW file sorting program in terminal",
-	Long:  `Photopic is JPG, RAW file sorting program in terminal`,
+	Long: `    ____  __          __              _     
+   / __ \/ /_  ____  / /_____  ____  (_)____
+  / /_/ / __ \/ __ \/ __/ __ \/ __ \/ / ___/
+ / ____/ / / / /_/ / /_/ /_/ / /_/ / / /__  
+/_/   /_/ /_/\____/\__/\____/ .___/_/\___/  
+                           /_/              v1.0.0
+Photopic is JPG, RAW file sorting program in terminal`,
 }
 
 func Execute() {
